241476ab1fdbec518b273b6f20157ca3223d3cf5
Maxim <hype746@gmail.com>
2025-12-03 17:56:48 +0300

    feat: add rand generate and show functions

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 document.getElementById("password1").addEventListener("click",()=>copy(1));
 document.getElementById("password2").addEventListener("click",()=>copy(2));
+document.getElementById("generate-button").addEventListener("click",()=>showPasswords());
+const characters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+let password1 = document.getElementById("password1-text");
+let password2 = document.getElementById("password2-text");
 
 function copy(n){
     let id="password"+n+"-text";
     const text = document.getElementById(id).textContent;
     navigator.clipboard.writeText(text)
 }
-function generatePassword{
-    
+function showPasswords(){
+    password1.innerHTML = generatePassword(15);
+    password2.innerHTML = generatePassword(15);
 }
-function showPasswords{
-
+function randomEl(masLength){
+    return Math.floor(Math.random()*masLength);
+}
+function generatePassword(len){
+    let password="";
+    for(let i=0;i<len;i++){
+        password+=characters[randomEl(characters.length)];
+    }
+    return password;
+    
 }
