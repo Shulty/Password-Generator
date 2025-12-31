5e170e7b7ffb3c7d451dd6ef2788252bf653a5d8
Maxim <hype746@gmail.com>
2025-12-31 19:09:20 +0300

    feat: add print animation

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ document.getElementById("password1").addEventListener("click",()=>copy(1));
 document.getElementById("password2").addEventListener("click",()=>copy(2));
 document.getElementById("generate-button").addEventListener("click",()=>showPasswords());
 const characters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-let password1 = document.getElementById("password1-text");
-let password2 = document.getElementById("password2-text");
+const password1 = document.getElementById("password1-text");
+const password2 = document.getElementById("password2-text");
 
 function copy(n){
     let id="password"+n+"-text";
@@ -11,8 +11,8 @@ function copy(n){
     navigator.clipboard.writeText(text)
 }
 function showPasswords(){
-    password1.innerHTML = generatePassword(15);
-    password2.innerHTML = generatePassword(15);
+    typeWriter(generatePassword(15),password1,30);
+    typeWriter(generatePassword(15),password2,30);
 }
 function randomEl(masLength){
     return Math.floor(Math.random()*masLength);
@@ -23,5 +23,23 @@ function generatePassword(len){
         password+=characters[randomEl(characters.length)];
     }
     return password;
-    
 }
+function typeWriter(text, element, speed = 100, i = 0) {
+     function stepErase() {
+         const txt = element.textContent;
+          if (txt.length > 0) {
+             element.textContent = txt.slice(0, -1);
+            setTimeout(stepErase, speed);
+         }else{
+            stepPrint();
+            return;
+         } }
+    function stepPrint(){
+             if (i < text.length) {
+         element.textContent += text[i];
+         i++;
+          setTimeout(stepPrint, speed); 
+        }
+    }
+         stepErase();
+     }
